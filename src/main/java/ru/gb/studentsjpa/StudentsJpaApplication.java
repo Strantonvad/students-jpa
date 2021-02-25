c29bb73d15fe3f260aba7bfd30621eff43674e51
Strantonvad <nottna@gmail.com>
2021-02-25 12:52:33 +0300

    init commit

---
+++ src/main/java/ru/gb/studentsjpa/StudentsJpaApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.studentsjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsJpaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudentsJpaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/gb/studentsjpa/StudentsJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.studentsjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsJpaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
